ccba21efdfbdf36f15eca330bfd3de8939d0063f
San Chui <chuisan3021@gmail.com>
2022-09-26 19:32:33 -0700

    Complete Bee

--- src/Bee.js
+++ src/Bee.js
@@ -3,9 +3,9 @@ var Bee = function() {
 
   this.age = 5;
   this.color = 'yellow';
-
+  this.job = 'keep on growing'
 };
 
-Bee.prototype = Object.creat(Grub.prototype);
+Bee.prototype = Object.create(Grub.prototype);
 Bee.prototype.constructor = Bee;
 
